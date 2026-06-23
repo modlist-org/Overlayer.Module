9bf9d7e0a0023b57850a462f93454fe88e6a2fd7
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2026-06-24 03:03:31 +0900

    Add Color Tag

---
+++ ADOFAI/Tag/Color.cs
@@ -0,0 +1,35 @@
+﻿using Overlayer.Tag.Core;
+
+namespace Overlayer.Module.ADOFAI.Tag;
+
+public static class Color {
+    [Tag(Desc = "\"Early!!\" Hex Color")]
+    public const string TEHex = "FF0000";
+
+    [Tag(Desc = "\"Early\" Hex Color")]
+    public const string VEHex = "FF6F4E";
+
+    [Tag(Desc = "\"EPerfect\" Hex Color")]
+    public const string EPHex = "FCFF4D";
+
+    [Tag(Desc = "\"Perfect!\" Hex Color")]
+    public const string PHex = "60FF4E";
+
+    [Tag(Desc = "\"LPerfect\" Hex Color")]
+    public const string LPHex = "FCFF4D";
+
+    [Tag(Desc = "\"Late\" Hex Color")]
+    public const string VLHex = "FF6F4E";
+
+    [Tag(Desc = "\"Late!!\" Hex Color")]
+    public const string TLHex = "FF0000";
+
+    [Tag(Desc = "\"Multipress\" Hex Color")]
+    public const string MultipressHex = "00FFED";
+
+    [Tag(Desc = "\"Miss...\" Hex Color")]
+    public const string MissHex = "D958FF";
+
+    [Tag(Desc = "\"Overload...\" Hex Color")]
+    public const string OverloadHex = "D958FF";
+}

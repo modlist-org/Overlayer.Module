2a35c7915285a45f18b8aacb554ba3fa4886f2eb
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2026-08-09 18:09:26 +0900

    1.0.1

--- ADOFAI/Info.cs
+++ ADOFAI/Info.cs
@@ -3,5 +3,5 @@ namespace Overlayer.Module.ADOFAI;
 public static class Info {
     public const string Name = "ADOFAI";
     public const string Author = "modlist.org";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
 }
